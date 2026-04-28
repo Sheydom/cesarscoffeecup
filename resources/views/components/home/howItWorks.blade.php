635759eba4b79c50822946101d04cc770b4040fa
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-28 23:18:44 +1000

    home layout howitworks completed

--- resources/views/components/home/howItWorks.blade.php
+++ resources/views/components/home/howItWorks.blade.php
@@ -1,27 +1,37 @@
-<section class="p-5 bg-neutral mt-10">
-    <h2 class="text-4xl text-center font-bodoni mb-10">How it works</h2>
+<section class="p-5 bg-primary mt-10">
+    <h2 class="text-4xl text-center font-bodoni mb-10 text-white">How it works</h2>
     <div class="flex flex-col mb-10">
-        <p class="text-6xl text-primary/70">01</p>
-        <div class="flex flex-col items-center -mt-5">
-            <h3 class="text-2xl mb-5">Book your visit</h3>
-            <p class="text-center">Tour our North Williamstown facility and meet the team behind Melbourne's finest
-                roasts.</p>
+        <div class="flex -mt-5">
+            <p class="text-2xl flex p-2 h-max mr-5  text-black w-max font-bodoni italic bg-pastel rounded-full  ">
+                1</p>
+            <div class="flex flex-col">
+                <h3 class="text-2xl mb-5 text-white font-bodoni italic">Book your visit</h3>
+                <p class=" text-pastel">Tour our North Williamstown facility and meet the team behind Melbourne's
+                    finest
+                    roasts.</p>
+            </div>
         </div>
     </div>
     <div class="flex flex-col mb-10">
-        <p class="text-6xl flex justify-end text-primary/70">02</p>
-        <div class="flex flex-col items-center -mt-5">
-            <h3 class="text-2xl mb-5">Explore & taste</h3>
-            <p class="text-center">Tour our North Williamstown facility and meet the team behind Melbourne's finest
-                roasts.</p>
+        <div class="flex -mt-5 ">
+            <p
+                class="text-2xl p-2 h-max mr-5  border-l-pastel border-l-2 text-black font-bodoni italic bg-pastel rounded-full  ">
+                2</p>
+            <div class="flex flex-col">
+                <h3 class="text-2xl mb-5 text-white font-bodoni italic">Explore & taste</h3>
+                <p class=" text-pastel">Sample grean beans from our partner farms in Colombia, Ethiopia and Brazil.</p>
+            </div>
         </div>
     </div>
-    <div class="flex flex-col">
-        <p class="text-6xl text-primary/70">03</p>
-        <div class="flex flex-col items-center -mt-5">
-            <h3 class="text-2xl mb-5">Create your coffee</h3>
-            <p class="text-center">Tour our North Williamstown facility and meet the team behind Melbourne's finest
-                roasts.</p>
+    <div class="flex flex-col mb-10">
+        <div class="flex -mt-5">
+            <p class="text-2xl flex p-2 h-max mr-5  text-black w-max font-bodoni italic bg-pastel rounded-full  ">
+                3</p>
+            <div class="flex flex-col">
+                <h3 class="text-2xl mb-5 text-white font-bodoni italic">Create your coffee</h3>
+                <p class=" text-pastel">Finalize your profile, choose your packaging, and launch your brand to market.
+                </p>
+            </div>
         </div>
     </div>
 </section>
