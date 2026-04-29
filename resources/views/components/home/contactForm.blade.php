82ed42f5f41b8edeeff434b5dcc0f92c24f8d3e5
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-29 20:57:49 +1000

    home-contactform completed, also added css apply p leading relaxed

--- resources/views/components/home/contactForm.blade.php
+++ resources/views/components/home/contactForm.blade.php
@@ -11,6 +11,22 @@
             <p class="text-coffeDark text-xs">FOUNDER & MASTER ROASTER</p>
         </div>
     </div>
+    <div class="flex gap-5 items-start">
+        <svg class="shrink-0 w-7 h-7 text-coffeDark" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 640 640"><!--!Font Awesome Pro v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2026 Fonticons, Inc.-->
+            <path fill="currentColor"
+                d="M128 252.6C128 148.4 214 64 320 64C426 64 512 148.4 512 252.6C512 371.9 391.8 514.9 341.6 569.4C329.8 582.2 310.1 582.2 298.3 569.4C248.1 514.9 127.9 371.9 127.9 252.6zM320 320C355.3 320 384 291.3 384 256C384 220.7 355.3 192 320 192C284.7 192 256 220.7 256 256C256 291.3 284.7 320 320 320z" />
+        </svg>
+        <p>19 Churchill St, North Williamstown,<br> VIC 3016</p>
+    </div>
+    <div class="flex gap-5 mb-10 items-start">
+        <svg class="shrink-0 h-7 w-7 text-coffeDark" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+            <path fill="currentColor"
+                d="M112 128C85.5 128 64 149.5 64 176C64 191.1 71.1 205.3 83.2 214.4L291.2 370.4C308.3 383.2 331.7 383.2 348.8 370.4L556.8 214.4C568.9 205.3 576 191.1 576 176C576 149.5 554.5 128 528 128L112 128zM64 260L64 448C64 483.3 92.7 512 128 512L512 512C547.3 512 576 483.3 576 448L576 260L377.6 408.8C343.5 434.4 296.5 434.4 262.4 408.8L64 260z" />
+        </svg>
+        <p>hello@cesarscoffeecup.com.au</p>
+    </div>
     <form class=" action="">
         <div class="flex flex-col mb-5"><label for="">Full Name</label><input
                 class="p-2.5 bg-neutral border-b-2 border-black" type="text" placeholder="e.g Julian Smith"></div>
@@ -29,6 +45,6 @@
         <div class="flex flex-col mb-5"><label for="">Your Message</label>
             <textarea class="resize-none mb-5 p-2.5 bg-neutral border-b-2 border-black" rows="5" cols="40"
                 placeholder="Type here..."></textarea>
-                <x-ui.buttonSolid class=" text-white">SUBMIT REQUEST</x-ui.buttonSolid>
+            <x-ui.buttonSolid class=" text-white">SUBMIT REQUEST</x-ui.buttonSolid>
     </form>
 </section>
