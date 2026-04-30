c1d5836e0b3836ee1f23ca10746ab53002da8eb1
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-30 21:45:21 +1000

    hero section img shift

--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -1,5 +1,5 @@
 <section id="hero" class="relative flex flex-col items-center justify-end">
-    <img class=" object-cover " src="{{ asset('images/hero.jpg') }}" alt="coffePlantage">
+    <img class=" object-cover overflow-hidden " src="{{ asset('images/hero.jpg') }}" alt="coffePlantage">
     <div class=" absolute inset-0 bg-accent/35"></div>
     <div class="z-10 mb-10 absolute flex flex-col  items-center  text-white w-[90%] ">
         <h1 class=" mb-10 text-5xl font-bodoni italic">Build your own <br> coffee brand <br> in Melbourne</h1>
