aa7c547e1ff384a3e19b88146b799fba9a06ea21
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-30 21:25:09 +1000

    gpu transform helper for burger menu slide

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -34,7 +34,7 @@
                 x-transition:enter-start="-translate-x-full opacity-0" x-transition:enter-end="translate-x-0"
                 x-transition:leave="transition ease-in duration-300 " x-transition:leave-start="translate-x-0"
                 x-transition:leave-end="-translate-x-full opacity-0"
-                class="fixed top-0 left-0 h-full w-72 bg-[#FCF9F2] z-50  p-6">
+                class="fixed top-0 left-0 h-full w-72 bg-[#FCF9F2] z-50  p-6 transform-gpu will-change-transform">
 
                 <button @click="open = false" class="mb-6 ">
 
