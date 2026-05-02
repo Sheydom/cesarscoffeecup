49e966e81233990088d99a07c78b8794c51e66a5
Dominic Knabe <dominic.knabe@gmail.com>
2026-05-02 20:02:44 +1000

    header style

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="flex text-black p-2 justify-between w-full ">
+<nav class="flex bg-[#FCF9F2] text-black p-2 justify-between w-full font-medium ">
 
     <div class="sm:hidden self-start">
         <div x-data="{ open: false }"class="relative">
@@ -15,14 +15,14 @@
                     x-transition:leave-end="opacity-0 scale-90 rotate-90"
                     class="absolute w-10 h-10 shrink-0 cursor-pointer" xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 640 640">
-                    <path fill="rgb(5, 5, 5)"
+                    <path fill="currentColor"
                         d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z" />
                 </svg>
 
             </button>
             {{-- Dark overlay --}}
 
-             {{-- <div x-show="open" @click="open = false" x-transition.opacity class="fixed inset-0 bg-black/50 z-40"></div>  --}}
+            {{-- <div x-show="open" @click="open = false" x-transition.opacity class="fixed inset-0 bg-black/50 z-40"></div>  --}}
 
             {{-- Sliding sidebar --}}
 
@@ -46,14 +46,16 @@
 
                 <nav class="flex flex-col gap-4 text-lg">
 
-                    <a class=" hover:bg-accent/10 rounded p-2 {{request()->routeIs('home')?'text-primary underline underline-primary font-bold scale-105':'text-black/30 hover:text-primary transition duration-300 ease-in-out'}}" href="{{ route('home') }} "wire:navigate.hover>Home</a>
-                    <a class="hover:bg-accent/10 rounded p-2 {{request()->routeIs('coRoasting')?'text-primary underline underline-primary font-bold scale-105':'text-black/30 hover:text-primary transition duration-300 ease-in-out'}}"
+                    <a class=" hover:bg-accent/10 rounded p-2 {{ request()->routeIs('home') ? 'text-primary underline underline-primary font-bold scale-105' : 'text-black/30 hover:text-primary transition duration-300 ease-in-out' }}"
+                        href="{{ route('home') }} "wire:navigate.hover>Home</a>
+                    <a class="hover:bg-accent/10 rounded p-2 {{ request()->routeIs('coRoasting') ? 'text-primary underline underline-primary font-bold scale-105' : 'text-black/30 hover:text-primary transition duration-300 ease-in-out' }}"
                         href="{{ route('coRoasting') }} "wire:navigate.hover>Co-Roasting</a>
-                    <a class="hover:bg-accent/10 rounded p-2 {{request()->routeIs('hireRoaster')?'text-primary underline underline-primary font-bold scale-105':'text-black/30 hover:text-primary transition duration-300 ease-in-out'}}"
+                    <a class="hover:bg-accent/10 rounded p-2 {{ request()->routeIs('hireRoaster') ? 'text-primary underline underline-primary font-bold scale-105' : 'text-black/30 hover:text-primary transition duration-300 ease-in-out' }}"
                         href="{{ route('hireRoaster') }} "wire:navigate.hover>Hire a Roaster</a>
-                    <a class="hover:bg-accent/10 rounded p-2 {{request()->routeIs('aboutUs')?'text-primary underline underline-primary font-bold scale-105':'text-black/30 hover:text-primary transition duration-300 ease-in-out'}}" href="{{ route('aboutUs') }}"wire:navigate.hover>About
+                    <a class="hover:bg-accent/10 rounded p-2 {{ request()->routeIs('aboutUs') ? 'text-primary underline underline-primary font-bold scale-105' : 'text-black/30 hover:text-primary transition duration-300 ease-in-out' }}"
+                        href="{{ route('aboutUs') }}"wire:navigate.hover>About
                         Us</a>
-                    <a class="hover:bg-accent/10 rounded p-2 {{request()->routeIs('contact')?'text-primary underline underline-primary font-bold scale-105':'text-black/30 hover:text-primary transition duration-300 ease-in-out'}}"
+                    <a class="hover:bg-accent/10 rounded p-2 {{ request()->routeIs('contact') ? 'text-primary underline underline-primary font-bold scale-105' : 'text-black/30 hover:text-primary transition duration-300 ease-in-out' }}"
                         href="{{ route('contact') }}"wire:navigate.hover>Contact</a>
 
                 </nav>
@@ -95,20 +97,25 @@
         </div>
     </div>
 
-    <div class="h-10 flex items-center">
+    <div class="h-10 flex items-center md:text-xl">
         <p class="text-center font-bold">Cesar's Coffee Cup</p>
     </div>
-    <div class="hidden sm:flex sm:gap-4 md:gap-10 sm:justify-around sm:items-center">
-        <a href="{{ route('home') }}"class="{{request()->routeIs('home')?'text-primary underline underline-primary font-bold scale-105':'text-black/30 hover:text-primary transition duration-300 ease-in-out'}}" wire:navigate.hover>Home</a>
-        <a href="{{ route('coRoasting') }}" class="{{request()->routeIs('coRoasting')?'text-primary underline underline-primary font-bold scale-105':'text-black/30 hover:text-primary transition duration-300 ease-in-out'}}"wire:navigate.hover>Co-Roasting</a>
-        <a href="{{route('hireRoaster')}}"class="{{request()->routeIs('hireRoaster')?'text-primary underline underline-primary font-bold scale-105':'text-black/30 hover:text-primary transition duration-300 ease-in-out'}}"wire:navigate.hover>Hire a Roaster</a>
-        <a href="{{route('aboutUs')}}"class="{{request()->routeIs('aboutUs')?'text-primary underline underline-primary font-bold scale-105':'text-black/30 hover:text-primary transition duration-300 ease-in-out'}}"wire:navigate.hover>About Us</a>
-        <a href="{{ route('contact') }}"class="{{request()->routeIs('contact')?'text-primary underline underline-primary font-bold scale-105':'text-black/30 hover:text-primary transition duration-300 ease-in-out'}}"wire:navigate.hover>Contact</a>
+    <div class="hidden sm:flex sm:gap-4 md:gap-10 md:text-xl sm:justify-around sm:items-center">
+        <a href="{{ route('home') }}"class="{{ request()->routeIs('home') ? 'text-primary underline' : 'text-black/30 hover:text-primary hover:-translate-y-1 transition duration-300 ease-in-out' }}"
+            wire:navigate.hover>Home</a>
+        <a href="{{ route('coRoasting') }}"
+            class="{{ request()->routeIs('coRoasting') ? 'text-primary underline' : 'text-black/30 hover:text-primary hover:-translate-y-1 transition duration-300 ease-in-out' }}"wire:navigate.hover>Co-Roasting</a>
+        <a href="{{ route('hireRoaster') }}"class="{{ request()->routeIs('hireRoaster') ? 'text-primary underline ' : 'text-black/30 hover:text-primary hover:-translate-y-1 transition duration-300 ease-in-out' }}"
+            wire:navigate.hover>Hire a Roaster</a>
+        <a href="{{ route('aboutUs') }}"class="{{ request()->routeIs('aboutUs') ? 'text-primary underline ' : 'text-black/30 hover:text-primary hover:-translate-y-1 transition duration-300 ease-in-out' }}"
+            wire:navigate.hover>About Us</a>
+        <a href="{{ route('contact') }}"class="{{ request()->routeIs('contact') ? 'text-primary underline ' : 'text-black/30 hover:text-primary hover:-translate-y-1 transition duration-300 ease-in-out' }}"
+            wire:navigate.hover>Contact</a>
     </div>
     <div class="h-10 flex items-center">
         <svg class="h-10 w-10 shrink-0 cursor-pointer" xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-            <path fill="rgb(5, 5, 5)"
+            <path fill="currentColor"
                 d="M320 312C386.3 312 440 258.3 440 192C440 125.7 386.3 72 320 72C253.7 72 200 125.7 200 192C200 258.3 253.7 312 320 312zM290.3 368C191.8 368 112 447.8 112 546.3C112 562.7 125.3 576 141.7 576L498.3 576C514.7 576 528 562.7 528 546.3C528 447.8 448.2 368 349.7 368L290.3 368z" />
         </svg>
     </div>
