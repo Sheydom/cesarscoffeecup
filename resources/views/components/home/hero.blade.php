b270ab4d90d848d8c695d8b8c26c4a2c41020fe6
Dominic Knabe <dominic.knabe@gmail.com>
2026-05-02 21:30:24 +1000

    test

--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -1,20 +1,21 @@
 <section id="hero" class="relative flex flex-col items-center   overflow-hidden sm:justify-center">
     <img class="flex object-cover min-h-110 w-full  md:max-h-180" src="{{ asset('images/hero.jpg') }}" alt="coffePlantage">
     <div
-        class=" absolute inset-0 bg-linear-to-b from-black/60 to-black/1  sm:bg-linear-to-l sm:from-black/1 sm:to-black/90 ">
-    </div>
-    <div class="z-10 absolute flex flex-col  items-center sm:items-start  text-white w-[90%]  ">
-        <h1 class=" my-10 text-5xl font-bodoni italic sm:text-5xl lg:text-8xl">Specialty Coffee, <br> Built for Your
-            Business</h1>
-        <p class="mb-20 text-xl">Create, roast, and package your own coffee with flexible co-roasting and roaster hire in
-            our Melbourne facility</p>
-        <div class="flex flex-col gap-5 mb-5 w-full sm:flex-row sm:w-max ">
-            <x-ui.buttonSolid class="sm:w-max ">BOOK A FACILITY VISIT</x-ui.buttonSolid>
-            <x-ui.buttonClear class="sm:w-max">EXPLORE CO-ROASTING</x-ui.buttonClear>
+        class="flex flex-col items-center absolute inset-0 bg-linear-to-b from-black/60 to-black/1  sm:bg-linear-to-l sm:from-black/1 sm:to-black/90 ">
+        <div class="z-10 h-full absolute flex flex-col items-center sm:items-start sm:justify-center  text-white w-[90%]  ">
+            <h1 class=" my-10 text-5xl font-bodoni italic sm:text-5xl lg:text-8xl">Specialty Coffee, <br> Built for Your
+                Business</h1>
+            <p class="mb-10 text-xl">Create, roast, and package your own coffee with flexible co-roasting and roaster
+                hire in
+                our Melbourne facility</p>
+            <div class="flex flex-col gap-5 mb-5 w-full sm:flex-row sm:w-max ">
+                <x-ui.buttonSolid class="sm:w-max ">BOOK A FACILITY VISIT</x-ui.buttonSolid>
+                <x-ui.buttonClear class="sm:w-max">EXPLORE CO-ROASTING</x-ui.buttonClear>
+            </div>
         </div>
     </div>
-    <div
-        class="bg-accent  w-full text-white z-100 text-center text-xs md:text-sm ">
+
+    <div class="bg-accent  w-full text-white z-100 text-center text-xs md:text-sm ">
         <p class="sm:hidden p-2  ">DIRECT FROM ORIGIN MELBOURNE-BASED</p>
         <div class="hidden sm:flex sm:flex-row sm:justify-around md:justify-around p-2  ">
             <p>Direct from Colombian & global farms</p>
