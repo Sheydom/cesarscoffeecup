654b885f59c6dbe0fbdcd54d7a66eb7bb573d036
Dominic Knabe <dominic.knabe@gmail.com>
2026-05-02 21:03:56 +1000

    style

--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -8,14 +8,22 @@
             Business</h1>
         <p class="mb-20 text-xl">Create, roast, and package your own coffee with flexible co-roasting and roaster hire in
             our Melbourne facility</p>
-        <div class="flex flex-col gap-5 mb-10 w-full sm:flex-row sm:w-max ">
+        <div class="flex flex-col gap-5 mb-5 w-full sm:flex-row sm:w-max ">
             <x-ui.buttonSolid class="sm:w-max ">BOOK A FACILITY VISIT</x-ui.buttonSolid>
             <x-ui.buttonClear class="sm:w-max">EXPLORE CO-ROASTING</x-ui.buttonClear>
         </div>
     </div>
-    <div class="bg-accent w-full text-white z-100 text-center text-xs md:text-sm">
+    <div
+        class="bg-accent  w-full text-white z-100 text-center text-xs md:text-sm ">
         <p class="sm:hidden p-2  ">DIRECT FROM ORIGIN MELBOURNE-BASED</p>
-        <p class="hidden sm:block p-2  ">Direct from Colombian & global farms · Loring
-            & IMF equipment · No lock‑in contracts · Melbourne local delivery</p>
+        <div class="hidden sm:flex sm:flex-row sm:justify-around md:justify-around p-2  ">
+            <p>Direct from Colombian & global farms</p>
+            <p>
+                Loring
+                & IMF equipment
+            </p>
+            <p>No lock-in contracts </p>
+            <p>Melbourne local delivery</p>
+        </div>
     </div>
 </section>
