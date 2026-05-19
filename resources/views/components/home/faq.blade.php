879ca593b973d1ae57331ea55f68fe765e39ca05
Dominic Knabe <dominic.knabe@gmail.com>
2026-05-19 22:13:08 +1000

    faq all sites evened out space gap-5

--- resources/views/components/home/faq.blade.php
+++ resources/views/components/home/faq.blade.php
@@ -2,7 +2,7 @@
     <h2 class="mb-5 text-4xl md:text-5xl lg:text-7xl md:mb-20 font-bodoni italic text-primary text-center">Questions of
         Origin</h2>
     <div class="flex flex-col gap-5 md:flex-row md:justify-center md:gap-20 lg:gap-20">
-        <div class="flex flex-col gap-5 md:max-w-md lg:gap-10 lg:max-w-xl lg:w-full">
+        <div class="flex flex-col gap-5 md:max-w-md lg:gap-5 lg:max-w-xl lg:w-full">
             <div x-data="{ open: false }" @click.outside="open = false" class="group flex flex-col cursor-pointer">
                 <button type="button" @click="open = !open"
                     class="flex justify-between w-full items-center text-left font-bold">
