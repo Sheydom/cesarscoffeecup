164325282d6bcf305a82c6c18cd316a90ebc27e7
Dominic Knabe <dominic.knabe@gmail.com>
2026-05-26 22:27:37 +1000

    starter kit reset password change logo to ccc

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -5,13 +5,15 @@
 @if($sidebar)
     <flux:sidebar.brand name="Laravel Starter Kit" {{ $attributes }}>
         <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-            <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+             <x-app-logo-icon class="size-5 fill-current text-black dark:text-white" />
+            {{-- <img src="{{asset('images/Cesars_Coffee_Cup_just_face.png')}}" alt="Cesars Coffe Cup Logo"> --}}
         </x-slot>
     </flux:sidebar.brand>
 @else
     <flux:brand name="Laravel Starter Kit" {{ $attributes }}>
         <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-            <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+            <x-app-logo-icon class="size-10 fill-current text-black dark:text-white" /> 
+            {{-- <img src="{{asset('images/Cesars_Coffee_Cup_just_face.png')}}" alt="Cesars Coffe Cup Logo"> --}}
         </x-slot>
     </flux:brand>
 @endif
