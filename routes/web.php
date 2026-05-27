582baa16b158d4f283bc73ea1b8d19cbb231761c
Dominic Knabe <dominic.knabe@gmail.com>
2026-05-27 19:56:32 +1000

    Fix wrong setting route

--- routes/web.php
+++ routes/web.php
@@ -58,4 +58,4 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
+//require __DIR__.'/settings.php';
