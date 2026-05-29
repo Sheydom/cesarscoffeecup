692fecc1422af1490230159c912b660ffca50778
Dominic Knabe <dominic.knabe@gmail.com>
2026-05-30 08:04:16 +1000

    added contact email button on wholesale page'

--- resources/views/components/wholesale/pdfIframe.blade.php
+++ resources/views/components/wholesale/pdfIframe.blade.php
@@ -1,5 +1,5 @@
-<section class="flex justify-center px-4 my-20">
-    <div class="w-full max-w-5xl p-2 sm:p-6 md:p-10 bg-tertiary rounded-2xl">
+<section class="flex justify-center px-4 my-20 flex-col items-center">
+    <div class="w-full max-w-4xl p-2 sm:p-6 md:p-6 bg-tertiary rounded-2xl">
         <iframe
             class="w-full h-150 sm:h-200 rounded-2xl border-0"
             loading="lazy"
@@ -7,4 +7,5 @@
             src="{{ asset('images/wholesale/wholesale-responsive.html') }}">
         </iframe>
     </div>
+    <a href="mailto:info@cesarscoffeecup.com?subject=Wholesale%20Enquiry" class="flex gap-2 items-center border-2 rounded-2xl mt-5 px-10 py-2.5 hover:border-black transition duration-300 ease-in-out"><svg class="w-7 h-7 shrink-0" viewBox="0 -2.5 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>email [#1573]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-300.000000, -922.000000)" fill="#000000"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M262,764.291 L254,771.318 L246,764.281 L246,764 L262,764 L262,764.291 Z M246,775 L246,766.945 L254,773.98 L262,766.953 L262,775 L246,775 Z M244,777 L264,777 L264,762 L244,762 L244,777 Z" id="email-[#1573]"> </path> </g> </g> </g> </g></svg> Contact our Wholesale Team</a>
 </section>
