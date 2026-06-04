709c1b0649de43d10fd3a96536eb22847d997637
Dominic Knabe <dominic.knabe@gmail.com>
2026-06-04 18:56:32 +1000

    changed favicon for browser tab to white background black stroke

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    <link rel="icon" type="image/png" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/Cesars_Coffee_Cup_just_face_black.png') }}">
 
     <meta property="og:type" content="website">
 
